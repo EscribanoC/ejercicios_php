44f28bef8c4298d0cb23d366e8392d2554f9d149
EscribanoC <escribano.flores.carlos@iescamas.es>
2024-09-23 19:12:02 +0200

    Ejercicio3

--- ejercicio1/saludo.php
+++ ejercicio1/saludo.php
@@ -1,6 +1,5 @@
 <?php
-    $nombre = $_POST['nombre'];
-    $apellidos = $_POST['apellidos'];
-
-    echo "Mi nombre es $nombre $apellidos";
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+echo "Mi nombre es $nombre $apellidos";
 ?>
