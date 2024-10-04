2a6adc55c74a9f8fc5bd288a8a89d8239762dce7
EscribanoC <escribano.flores.carlos@iescamas.es>
2024-10-04 08:46:13 +0200

    Actualización Ejercicio 7

--- ejercicio7/tablaMultiplicar.php
+++ ejercicio7/tablaMultiplicar.php
@@ -1,27 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tabla de multiplicar</title>
     <link rel="stylesheet" href="../EjerciciosPHP.css">
 </head>
+
 <body>
     <div>
         <?php
-            $numero = $_GET['numero'];
-            echo "<h3>Tabla de multiplicar del $numero</h3>";
-            echo "<p>$numero x 1 = " . $numero *1 . "</p>";
-            echo "<p>$numero x 2 = " . $numero *2 . "</p>";
-            echo "<p>$numero x 3 = " . $numero *3 . "</p>";
-            echo "<p>$numero x 4 = " . $numero *4 . "</p>";
-            echo "<p>$numero x 5 = " . $numero *5 . "</p>";
-            echo "<p>$numero x 6 = " . $numero *6 . "</p>";
-            echo "<p>$numero x 7 = " . $numero *7 . "</p>";
-            echo "<p>$numero x 8 = " . $numero *8 . "</p>";
-            echo "<p>$numero x 9 = " . $numero *9 . "</p>";
-            echo "<p>$numero x 10 = " . $numero *10 . "</p>";
-        ?>
+        if (isset($_GET["numero"])) {
+            if (is_numeric($_GET["numero"])) {
+                $numero = $_GET['numero'];
+                echo "<h3>Tabla de multiplicar del $numero</h3>";
+                echo "<table>";
+                for($i = 1; $i<=10;$i++){
+                    $mult = $num*$i;
+                    echo "<tr>";
+                    echo "<p>$numero x " . $i . " = " . $numero * $i . "</p>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            }
+        }
+        ?> 
     </div>
 </body>
+
 </html>
